c620f046c4e44bb525efd60fd6035dcf96de7fd7
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-29 12:58:06 -0300

    feat: cria classe FilmeRepositorio

---
+++ DIO.Series/Entities/FilmeRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using DIO.Series.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DIO.Series.Entities
+{
+    public class FilmeRepositorio : IRepositorio<Filme>
+    {
+        private List<Filme> listaSerie = new List<Filme>();
+
+        public void Atualizar(int id, Filme objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Inserir(Filme objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Filme> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count();
+        }
+
+        public Filme RetornarPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
