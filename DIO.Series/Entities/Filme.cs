36d678c5f0e65cfb55b565141c15d6148a9ff138
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-29 12:57:51 -0300

    feat: cria classe Filme

---
+++ DIO.Series/Entities/Filme.cs
@@ -0,0 +1,55 @@
+﻿using DIO.Series.Enums;
+using System.Text;
+
+namespace DIO.Series.Entities
+{
+    public class Filme : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Ativo { get; set; }
+
+        public Filme(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Ativo = true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Gênero: {Genero}");
+            stringBuilder.AppendLine($"Titulo: {Titulo}");
+            stringBuilder.AppendLine($"Descrição: {Descricao}");
+            stringBuilder.AppendLine($"Ano de Lançamento: {Ano}");
+            stringBuilder.AppendLine($"Ativo: {Ativo}");
+            return stringBuilder.ToString();
+        }
+
+        public string RetornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return Id;
+        }
+
+        public void Excluir()
+        {
+            Ativo = false;
+        }
+
+        public bool RetornaExcluido()
+        {
+            return Ativo;
+        }
+    }
+}
