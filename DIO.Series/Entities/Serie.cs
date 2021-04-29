e95c484179e331548d56b92c0eba0afe4e97d8db
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-29 08:32:47 -0300

    feat: adiciona classe Serie

---
+++ DIO.Series/Entities/Serie.cs
@@ -0,0 +1,43 @@
+﻿using DIO.Series.Entities;
+using DIO.Series.Enums;
+using System.Text;
+
+namespace DIO.Entities
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Gênero: {Genero}");
+            stringBuilder.AppendLine($"Titulo: {Titulo}");
+            stringBuilder.AppendLine($"Descrição: {Descricao}");
+            stringBuilder.AppendLine($"Ano de Lançamento: {Ano}");
+            return stringBuilder.ToString();
+        }
+
+        public string RetornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return Id;
+        }
+    }
+}
