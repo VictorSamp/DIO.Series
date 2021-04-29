a8c324bb65330153e4a609f4a88f0895f3622068
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-29 08:36:54 -0300

    feat: cria interface IRepositorio e adiciona metodos obrigatorios

---
+++ DIO.Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DIO.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        internal List<T> Lista();
+
+        internal T RetornaPorId();
+
+        internal void Insere(T entidade);
+
+        internal void Exclui(int id);
+
+        internal void Atualiza(int id, T entidade);
+
+        internal int ProximoId();
+    }
+}
