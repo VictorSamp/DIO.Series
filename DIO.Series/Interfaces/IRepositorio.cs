ccafbad98961e6ec3ba19eeac38c1feb2f09605c
VictorSamp <victor.msampaio@outlook.com.br>
2021-04-29 08:53:31 -0300

    refactor: altera para public o encapsulamento dos metodos da interface IRepositorio

--- DIO.Series/Interfaces/IRepositorio.cs
+++ DIO.Series/Interfaces/IRepositorio.cs
@@ -4,16 +4,16 @@ namespace DIO.Series.Interfaces
 {
     public interface IRepositorio<T>
     {
-        internal List<T> Lista();
+        public List<T> Lista();
 
-        internal T RetornaPorId(int id);
+        public T RetornaPorId(int id);
 
-        internal void Insere(T entidade);
+        public void Insere(T entidade);
 
-        internal void Exclui(int id);
+        public void Exclui(int id);
 
-        internal void Atualiza(int id, T entidade);
+        public void Atualiza(int id, T entidade);
 
-        internal int ProximoId();
+        public int ProximoId();
     }
 }
